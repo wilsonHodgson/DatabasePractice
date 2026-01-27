4dbbbd69b5940d102e494253aea2b20aaf90d438
Wilson Hodgson-Hoffmeyer <hodgson.w@hotmail.com>
2026-01-26 22:17:56 -0500

    Adds sqlite3 as dependency, downloads the sakila sample database, configures a build task, and builds before launching

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <iterator>
+#include <sqlite3.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
 
 /*
 class Main{
@@ -10,5 +16,48 @@ static void main (int args[]){
 */
 
 int main(int, char**){
-    std::cout << "Hello, World!\n";
+
+    sqlite3*        db;
+    char            queue[999];
+    sqlite3_stmt*    stmt;
+    int             row = 0;
+    int             bytes;
+    const unsigned char*          text;
+
+    queue[sizeof queue - 1] = '\0';
+    snprintf(queue, sizeof queue - 1, "SELECT * FROM country");
+
+    if (sqlite3_open("/home/wilson/Projects/DatabasePractice/sakila_master.db", &db) != SQLITE_OK) {
+        cout << "Error opening database.";
+        return 2;
+    };
+
+    printf("Query: %s\n", queue);
+
+    sqlite3_prepare(db, queue, sizeof queue, &stmt, NULL);
+
+    bool done = false;
+    while (!done){
+        cout << printf("In select while\n");
+        switch (sqlite3_step(stmt)) {
+            case SQLITE_ROW:
+                bytes = sqlite3_column_bytes(stmt, 0);
+                text = sqlite3_column_text(stmt, 1);
+                cout << printf ("count %d: %s (%d bytes)\n", row, text, bytes);
+                row++;
+                break;
+
+            case SQLITE_DONE:
+                done = true;
+                break;
+            
+            default:
+                cout << fprintf(stderr, "Failed.\n");
+                return 1;
+        }
+
+    }
+   // sqlite3_exec(db, "SELECT ALL FROM country;");
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
 }
